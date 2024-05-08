4dd922d4ad9aca97ed7d9dc469fed0282536c223
skhaire449 <90052810+skhaire449@users.noreply.github.com>
2024-05-08 11:00:09 +0530

    Delete Assignment-3-B directory

--- Assignment-3-B/connectToDB/connectToDB.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const URL = 'mongodb+srv://Gaurav-WAD:oeOCnPDwbArJB3vg@cluster0.nq1mdcw.mongodb.net/'
-
-export const connectToDB = () =>{
-    mongoose.connect(URL)
-    console.log("Connected to DB")
-}
